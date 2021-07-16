2c23d4b10583289d3af605832386d1f226fc2abd
zelinanton <zelin2047@mail.ru>
2021-07-16 16:12:39 +0300

    main

---
+++ app.js
@@ -0,0 +1,10 @@
+$(function() {  
+    $("[data-slider]").slick({
+        dots: true,
+        dotsClass: "my-dots",
+        fade: false,
+        arrows: false,
+        slidesToShow: 1,
+        slidesToScroll: 1
+    });
+});
